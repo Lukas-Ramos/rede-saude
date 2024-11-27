d9d258a3558e3b4ffe98ce7022424d9ab2418809
lucas.ramos <113211755+Lukas-Ramos@users.noreply.github.com>
2024-11-27 16:13:17 -0300

    commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -16,6 +16,20 @@ declare let L: any;
 export class HomeComponent implements AfterViewInit {
   user = { name: 'João Silva' };
   estabelecimentos: any[] = [];
+  icons = [
+   'fas fa-store', 
+    'fas fa-utensils',
+    'fas fa-cogs', 
+    'fas fa-cocktail', 
+    'fas fa-shopping-cart', 
+    'fas fa-bread-slice',
+    'fas fa-concierge-bell', 
+    'fas fa-cutlery', 
+    'fas fa-burger', 
+    'fas fa-tshirt', 
+  ];
+  
+  iconGalleryVisible = false;
   novoEstabelecimento = { nome: '', imagem: '', latitude: null, longitude: null };
   private map: any;
   private tempMarker: any = null;
@@ -26,13 +40,21 @@ export class HomeComponent implements AfterViewInit {
   ngAfterViewInit() {
     this.initMap();
     this.carregarEstabelecimentos();
-    
   }
+
+  toggleIconGallery() {
+    this.iconGalleryVisible = !this.iconGalleryVisible; 
+  }
+
+  selectIcon(icon: string) {
+    this.novoEstabelecimento.imagem = icon; 
+    this.iconGalleryVisible = false; 
+  }
+
   carregarEstabelecimentos() {
     this.http.get<any[]>('http://localhost:3000/estabelecimentos').subscribe(
       (response) => {
         this.estabelecimentos = response;
-  
         this.atualizarMapaComEstabelecimentos(response);
       },
       (error) => {
@@ -40,6 +62,7 @@ export class HomeComponent implements AfterViewInit {
       }
     );
   }
+
   atualizarMapaComEstabelecimentos(estabelecimentos: any[]) {
     if (this.map) {
       this.map.eachLayer((layer: any) => {
@@ -47,12 +70,14 @@ export class HomeComponent implements AfterViewInit {
           this.map.removeLayer(layer);
         }
       });
+  
       estabelecimentos.forEach((estabelecimento) => {
         if (estabelecimento.latitude && estabelecimento.longitude) {
-          const icon = L.icon({
-            iconUrl: estabelecimento.imagem || 'default-image-url.png',
-            iconSize: [40, 40],  
-            className: 'rounded-marker',
+          const icon = L.divIcon({
+            className: 'fa ' + estabelecimento.imagem, 
+            iconSize: [80, 80],
+            iconAnchor: [20, 40], 
+            popupAnchor: [0, -40]  
           });
   
           L.marker([estabelecimento.latitude, estabelecimento.longitude], { icon })
@@ -64,99 +89,106 @@ export class HomeComponent implements AfterViewInit {
     }
   }
 
-initMap() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const userLat = position.coords.latitude;
-        const userLon = position.coords.longitude;
-        this.map = L.map('map').setView([userLat, userLon], 13);
+  initMap() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const userLat = position.coords.latitude;
+          const userLon = position.coords.longitude;
+          this.map = L.map('map').setView([userLat, userLon], 13);
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        }).addTo(this.map);
+          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          }).addTo(this.map);
 
-        L.marker([userLat, userLon])
-          .addTo(this.map)
-          .bindPopup('Você está aqui!')
-          .openPopup();
+          L.marker([userLat, userLon])
+            .addTo(this.map)
+            .bindPopup('Você está aqui!')
+            .openPopup();
 
-        this.map.on('click', (e: any) => {
-          const { lat, lng } = e.latlng;
+          this.map.on('click', (e: any) => {
+            const { lat, lng } = e.latlng;
 
-          this.novoEstabelecimento.latitude = lat;
-          this.novoEstabelecimento.longitude = lng;
+            this.novoEstabelecimento.latitude = lat;
+            this.novoEstabelecimento.longitude = lng;
 
-          if (this.tempMarker) {
-            this.map.removeLayer(this.tempMarker);
-          }
+            if (this.tempMarker) {
+              this.map.removeLayer(this.tempMarker);
+            }
 
-          this.tempMarker = L.marker([lat, lng], { draggable: true }).addTo(this.map);
+            const icon = L.divIcon({
+              className: this.novoEstabelecimento.imagem ? this.novoEstabelecimento.imagem : 'fa fa-map-marker',
+              iconSize: [40, 40],
+              iconAnchor: [20, 40],
+              popupAnchor: [0, -40]
+            });
 
-          this.tempMarker.bindPopup('Local Temporário').openPopup();
-        });
+            this.tempMarker = L.marker([lat, lng], { draggable: true, icon })
+              .addTo(this.map);
 
-        this.carregarEstabelecimentos();
-      },
-      (error) => {
-        console.error('Erro ao obter localização:', error);
-        this.map = L.map('map').setView([51.505, -0.09], 13); 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        }).addTo(this.map);
+            this.tempMarker.bindPopup('Local Temporário').openPopup();
+          });
 
-        this.carregarEstabelecimentos();
-      }
-    );
-  } else {
-    console.error('Geolocalização não é suportada por este navegador.');
+          this.carregarEstabelecimentos();
+        },
+        (error) => {
+          console.error('Erro ao obter localização:', error);
+          this.map = L.map('map').setView([51.505, -0.09], 13); 
+          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          }).addTo(this.map);
+  
+          this.carregarEstabelecimentos();
+        }
+      );
+    } else {
+      console.error('Geolocalização não é suportada por este navegador.');
+    }
   }
-}
 
   cadastrarEstabelecimento() {
     if (
       this.novoEstabelecimento.nome &&
-      this.novoEstabelecimento.imagem &&
+      this.novoEstabelecimento.imagem && 
       this.novoEstabelecimento.latitude &&
       this.novoEstabelecimento.longitude
     ) {
       const { nome, imagem, latitude, longitude } = this.novoEstabelecimento;
-  
-      const icon = L.icon({
-        iconUrl: imagem || 'default-image-url.png',
+
+      const icon = L.divIcon({
+        className: 'fa ' + imagem,  
         iconSize: [40, 40],
-        className: 'rounded-marker',
+        iconAnchor: [20, 40],
+        popupAnchor: [0, -40]
       });
-  
+
       const marker = L.marker([latitude, longitude], { icon })
         .addTo(this.map)
         .bindPopup(`<strong>${nome}</strong>`)
         .openPopup();
-  
-      this.isLoading = true; 
-  
+
+      this.isLoading = true;
+
       this.http.post('http://localhost:3000/estabelecimentos', this.novoEstabelecimento).subscribe(
         (response) => {
-          setTimeout(() => { 
+          setTimeout(() => {
             this.estabelecimentos.push({ ...this.novoEstabelecimento });
             this.novoEstabelecimento = { nome: '', imagem: '', latitude: null, longitude: null };
-  
-            this.isLoading = false; 
+
+            this.isLoading = false;
             this.atualizarMapaComEstabelecimentos(this.estabelecimentos);
             this.carregarEstabelecimentos();
-          }, 1000); 
+          }, 1000);
         },
         (error) => {
           console.error('Erro ao cadastrar estabelecimento', error);
-          this.isLoading = false; 
+          this.isLoading = false;
         }
       );
     } else {
       alert('Por favor, preencha todos os campos do estabelecimento.');
     }
   }
-  
-  
 
   abrirNoGoogleMaps(latitude: number, longitude: number) {
     if (latitude && longitude) {
@@ -176,7 +208,6 @@ initMap() {
     this.http.delete(`http://localhost:3000/estabelecimentos/${id}`).subscribe(
       () => {
         this.estabelecimentos = this.estabelecimentos.filter((est) => est.id !== id);
-  
         this.atualizarMapaComEstabelecimentos(this.estabelecimentos);
       },
       (error) => {
@@ -185,8 +216,7 @@ initMap() {
       }
     );
   }
-  
-    
+
   logout() {
     this.router.navigate(['/login']);
   }
